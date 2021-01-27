75043512d1b567d31f5401b914187d36c94e5174
myluxton <myluxton@gmail.com>
2021-01-27 16:00:58 +0700

    Menambah koding di file controller.js dan routes.js

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-    response.ok("Aplikasi Rest API Berjalan!")
+    response.ok("Aplikasi Rest API Berjalan!", res)
+};
+
+//menampilkan semua data
+exports.tampilSemua = function(req, res) {
+    connection.query('SELECT * FROM tbl_user', function(error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
