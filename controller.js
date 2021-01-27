d569c7f5bd89dd0468c6e56c550c07f902551007
myluxton <myluxton@gmail.com>
2021-01-27 16:08:26 +0700

    Menambah coding di file controller.js dan routes.js untuk menampilkan data berdasdarkan id

--- controller.js
+++ controller.js
@@ -11,7 +11,19 @@ exports.index = function(req, res) {
 exports.tampilSemua = function(req, res) {
     connection.query('SELECT * FROM tbl_user', function(error, rows, fields) {
         if (error) {
-            connection.log(error);
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data berdasarkan ID
+exports.tampilberdasarkanid = function(req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM tbl_user WHERE id = ?', [id], function(error, rows, fields) {
+        if (error) {
+            console.log(error);
         } else {
             response.ok(rows, res)
         }
